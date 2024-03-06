de05af3b93cbe2ebac0d2aaaf4d7d6eae0b07d8a
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-03-06 22:47:16 +0900

    train3

--- script/train.sh
+++ script/train.sh
@@ -6,14 +6,16 @@
 #scratch_vae_anomal_nomal_data_without_pe
 
 
-port_number=50005
+port_number=50003
 bench_mark="Tuft"
 obj_name='teeth_crop_onlyanormal'
 trigger_word='teeth'
 layer_name='layer_3'
 sub_folder="up_16_32_64"
-file_name="scratch_vae_anomal_data_with_pe"
+file_name="pretrained_vae_anomal_data_with_pe"
 #  --use_position_embedder
+# --vae_model_dir "/home/dreamyou070/SupervisedMED/result/Tuft/vae_train/train_vae_reconstruction_nomal_data/vae_models/vae_104.safetensors" \
+
 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
  --main_process_port $port_number ../train.py --log_with wandb \
  --output_dir "../../result/${bench_mark}/${layer_name}/${sub_folder}/${file_name}" \
@@ -21,7 +23,6 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
  --start_epoch 0 --max_train_epochs 60 \
  --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
  --data_path "../../../MyData/anomaly_detection/${bench_mark}" \
- --vae_model_dir "/home/dreamyou070/SupervisedMED/result/Tuft/vae_train/train_vae_reconstruction_nomal_data/vae_models/vae_104.safetensors" \
  --trigger_word "${trigger_word}" \
  --obj_name "${obj_name}" \
  --do_map_loss \
