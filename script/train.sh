45f7127b465e759ffa61fcb32644ce0c6474e5d7
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-03-06 21:18:18 +0900

    sh train.sh

--- script/train.sh
+++ script/train.sh
@@ -7,13 +7,13 @@
 
 port_number=50002
 bench_mark="Tuft"
-obj_name='teeth_crop'
+obj_name='teeth_crop_onlyanormal'
 trigger_word='teeth'
 layer_name='layer_3'
 sub_folder="up_16_32_64"
-file_name="pretrained_vae_anomal_nomal_data_without_pe"
+file_name="pretrained_vae_anomal_data_without_pe"
 
-accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_config \
+accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
  --main_process_port $port_number ../train.py --log_with wandb \
  --output_dir "../../result/${bench_mark}/${layer_name}/${sub_folder}/${file_name}" \
  --train_unet --train_text_encoder \
