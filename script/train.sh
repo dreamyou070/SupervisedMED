9f352d4df3b8259f363aba8f3340965fdbbb1f2d
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-03-06 21:51:44 +0900

    train7

--- script/train.sh
+++ script/train.sh
@@ -4,13 +4,13 @@
 #pretrained_vae_anomal_nomal_data_without_pe
 #scratch_vae_anomal_nomal_data_with_pe
 
-port_number=50008
+port_number=50007
 bench_mark="Tuft"
 obj_name='teeth_crop_onlyanormal'
 trigger_word='teeth'
 layer_name='layer_3'
 sub_folder="up_16_32_64"
-file_name="scratch_vae_anomal_data_without_pe"
+file_name="scratch_vae_anomal_data_with_pe"
 
 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
  --main_process_port $port_number ../train.py --log_with wandb \
@@ -28,4 +28,4 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
                     'up_blocks_3_attentions_2_transformer_blocks_0_attn2',]" \
  --attn_loss_weight 1.0 \
  --do_cls_train \
- --normal_weight 1
+ --normal_weight 1 --use_position_embedder
