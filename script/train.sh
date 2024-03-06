7f0d688bf96327b5d12cf298d5f19ca2b6dfd5e2
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-03-06 22:02:15 +0900

    train5

--- script/train.sh
+++ script/train.sh
@@ -1,25 +1,27 @@
 # !/bin/bash
 
-#pretrained_vae_anomal_nomal_data_with_pe
-#pretrained_vae_anomal_nomal_data_without_pe
+#scratch_vae_anomal_data_with_pe
+#scratch_vae_anomal_data_without_pe
 #scratch_vae_anomal_nomal_data_with_pe
 #scratch_vae_anomal_nomal_data_without_pe
 
-port_number=50004
+
+port_number=50005
 bench_mark="Tuft"
 obj_name='teeth_crop_onlyanormal'
 trigger_word='teeth'
 layer_name='layer_3'
 sub_folder="up_16_32_64"
-file_name="pretrained_vae_anomal_data_without_pe"
-#  --use_position_embedder
-accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_config \
+file_name="scratch_vae_anomal_data_with_pe"
+#
+accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
  --main_process_port $port_number ../train.py --log_with wandb \
  --output_dir "../../result/${bench_mark}/${layer_name}/${sub_folder}/${file_name}" \
  --train_unet --train_text_encoder \
  --start_epoch 0 --max_train_epochs 60 \
  --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
  --data_path "../../../MyData/anomaly_detection/${bench_mark}" \
+ --vae_model_dir "/home/dreamyou070/SupervisedMED/result/Tuft/vae_train/train_vae_reconstruction_nomal_data/vae_models/vae_104.safetensors"
  --trigger_word "${trigger_word}" \
  --obj_name "${obj_name}" \
  --do_map_loss \
@@ -28,4 +30,4 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_config \
                     'up_blocks_3_attentions_2_transformer_blocks_0_attn2',]" \
  --attn_loss_weight 1.0 \
  --do_cls_train \
- --normal_weight 1
+ --normal_weight 1 --use_position_embedder
