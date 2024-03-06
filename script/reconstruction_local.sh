f67eb5d612915a4adf4d297c792f999b198f88b7
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-03-07 05:58:09 +0900

    recon again

--- script/reconstruction_local.sh
+++ script/reconstruction_local.sh
@@ -23,4 +23,4 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_config \
  --trg_layer_list "['up_blocks_1_attentions_2_transformer_blocks_0_attn2',
                     'up_blocks_2_attentions_2_transformer_blocks_0_attn2',
                     'up_blocks_3_attentions_2_transformer_blocks_0_attn2',]" \
- --threds [0.5] --use_position_embedder \
+ --threds [0.5] --use_position_embedder --do_train_check
