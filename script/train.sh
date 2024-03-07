d8788a24c31e601e3c32addfccc97b9e53acd4c6
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-03-07 09:49:44 +0900

    train4

--- script/train.sh
+++ script/train.sh
@@ -1,16 +1,16 @@
 # !/bin/bash
 
-port_number=50003
+port_number=50004
 bench_mark="Tuft"
 obj_name='teeth_crop'
 trigger_word='teeth'
 layer_name='layer_3'
 sub_folder="up_16_32_64"
-file_name="3_pretrained_vae_anomal_normal_data_with_pe"
+file_name="3_pretrained_vae_anomal_normal_data_without_pe"
 
 anomal_source_path="../../../MyData/anomal_source"
 
-accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
+accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_config \
  --main_process_port $port_number ../train.py --log_with wandb \
  --output_dir "../../result/${bench_mark}/${layer_name}/${sub_folder}/${file_name}" \
  --train_unet --train_text_encoder --start_epoch 0 --max_train_epochs 60 \
@@ -18,7 +18,7 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
  --data_path "../../../MyData/anomaly_detection/${bench_mark}" \
  --trigger_word "${trigger_word}" \
  --obj_name "${obj_name}" \
- --do_map_loss --use_position_embedder \
+ --do_map_loss \
  --trg_layer_list "['up_blocks_1_attentions_2_transformer_blocks_0_attn2',
                     'up_blocks_2_attentions_2_transformer_blocks_0_attn2',
                     'up_blocks_3_attentions_2_transformer_blocks_0_attn2',]" \
