710e7892da083c2424e123c28ef5cc93b112b419
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-03-07 10:36:30 +0900

    train7

--- script/train.sh
+++ script/train.sh
@@ -1,12 +1,12 @@
 # !/bin/bash
 
-port_number=50006
+port_number=50007
 bench_mark="Tuft"
-obj_name='teeth_crop_onlyanormal'
+obj_name='teeth_crop'
 trigger_word='teeth'
 layer_name='layer_3'
 sub_folder="up_16_32_64"
-file_name="6_scratch_vae_anomal_data_withouy_pe"
+file_name="7_scratch_vae_anomal_normal_data_with_pe"
 
 anomal_source_path="../../../MyData/anomal_source"
 
@@ -23,4 +23,5 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
                     'up_blocks_2_attentions_2_transformer_blocks_0_attn2',
                     'up_blocks_3_attentions_2_transformer_blocks_0_attn2',]" \
  --do_attn_loss --attn_loss_weight 1.0 --do_cls_train --normal_weight 1 \
+ --use_position_embedder \
  --vae_pretrained_dir "/home/dreamyou070/SupervisedMED/result/Tuft/vae_train/train_vae_reconstruction_nomal_data/vae_models/vae_104.safetensors"
