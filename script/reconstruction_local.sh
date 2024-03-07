166529bcbb985c1a082906d2bb5cccd38c764074
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-03-07 12:11:59 +0900

    reconstruction

--- script/reconstruction_local.sh
+++ script/reconstruction_local.sh
@@ -1,18 +1,18 @@
 # !/bin/bash
 
-port_number=50003
+port_number=50022
 bench_mark="Tuft"
 obj_name='teeth_crop_onlyanormal'
 caption='teeth'
 layer_name='layer_3'
 sub_folder="up_16_32_64"
-file_name="1_pretrained_vae_anomal_data_with_pe"
+file_name="2_pretrained_vae_anomal_data_without_pe"
 
 # position_embedding_layer="down_blocks_0_attentions_0_transformer_blocks_0_attn1"
 # --d_dim 320 --use_position_embedder --position_embedding_layer ${position_embedding_layer} \
 # --use_position_embedder
 # --vae_model_dir "/home/dreamyou070/SupervisedMED/result/Tuft/vae_train/train_vae_reconstruction_nomal_data/vae_models/vae_104.safetensors"
-
+# --use_position_embedder
 accelerate launch --config_file ../../../gpu_config/gpu_0_config \
  --main_process_port $port_number ../reconstruction_local.py \
  --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
@@ -23,4 +23,4 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_config \
  --trg_layer_list "['up_blocks_1_attentions_2_transformer_blocks_0_attn2',
                     'up_blocks_2_attentions_2_transformer_blocks_0_attn2',
                     'up_blocks_3_attentions_2_transformer_blocks_0_attn2',]" \
- --threds [0.5] --use_position_embedder
+ --threds [0.5]
