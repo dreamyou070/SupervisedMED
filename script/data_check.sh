2f4766a4a6e37882caaea0fa062e1e7e612c87c8
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-03-08 19:20:56 +0900

    Update data_check.sh

--- script/data_check.sh
+++ script/data_check.sh
@@ -9,7 +9,7 @@ sub_folder="up_16_32_64"
 file_name="13_pretrained_vae_anomal_normal_data_with_pe_self_aug"
 
 python ../data_check.py --log_with wandb \
- --output_dir "../../result/${bench_mark}/${obj_name}/data_check_20240308_0.4_1" \
+ --output_dir "../../result/${bench_mark}/${obj_name}/data_check_20240308_0.6_1" \
  --train_unet --train_text_encoder --start_epoch 0 --max_train_epochs 30 \
  --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
  --data_path "../../../MyData/anomaly_detection/${bench_mark}" \
@@ -18,4 +18,4 @@ python ../data_check.py --log_with wandb \
  --min_perlin_scale 1 \
  --max_perlin_scale 4 \
  --max_beta_scale 1 \
- --min_beta_scale 0.4
+ --min_beta_scale 0.6
