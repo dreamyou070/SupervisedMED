aafb0af018965e495266300ed0a7e11461e79417
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-03-10 08:35:43 +0900

    Update train.sh

--- script/train.sh
+++ script/train.sh
@@ -2,11 +2,11 @@
 #
 port_number=50017
 bench_mark="Tuft"
-obj_name='teeth_20240308'
+obj_name='20240308_final'
 trigger_word='teeth'
 layer_name='layer_3'
 sub_folder="up_16_32_64"
-file_name="17_pretrained_vae_pe_xray_img_rgb_anomal"
+file_name="18_pretrained_vae_pe_xray_img_rgb_anomal"
 
 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
  --main_process_port $port_number ../train.py --log_with wandb \
@@ -25,5 +25,4 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
  --min_perlin_scale 1 \
  --max_perlin_scale 4 \
  --max_beta_scale 0.9 \
- --min_beta_scale 0.5 \
- --do_self_aug
+ --min_beta_scale 0.5
