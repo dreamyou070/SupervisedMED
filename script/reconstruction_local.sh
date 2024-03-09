fcc11ed12e0899be3393e0c72104fbaf047b934c
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-03-10 08:53:32 +0900

    Update reconstruction_local.sh

--- script/reconstruction_local.sh
+++ script/reconstruction_local.sh
@@ -1,12 +1,12 @@
 # !/bin/bash
 
-port_number=50019
+port_number=51919
 bench_mark="Tuft"
 obj_name='20240308_final'
 caption='teeth'
 layer_name='layer_3'
 sub_folder="up_16_32_64"
-file_name="17_pretrained_vae_pe_xray_anomal_dtd"
+file_name="15_pretrained_vae_anomal_with_pe_rgb_img_perlin_noise"
 
 # --use_position_embedder
 # --vae_pretrained_dir "/home/dreamyou070/SupervisedMED/result/Tuft/vae_train/train_vae_reconstruction_nomal_data/vae_models/vae_104.safetensors" \
@@ -22,4 +22,4 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_config \
  --trg_layer_list "['up_blocks_1_attentions_2_transformer_blocks_0_attn2',
                     'up_blocks_2_attentions_2_transformer_blocks_0_attn2',
                     'up_blocks_3_attentions_2_transformer_blocks_0_attn2',]" \
- --threds [0.5] --use_position_embedder --test_with_xray
+ --threds [0.5] --use_position_embedder
