8a6ed6e17e5eec4aed88d9bc29ba27f1c100e8e1
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-03-12 05:06:24 +0900

    train unsupervised

--- script/train.sh
+++ script/train.sh
@@ -1,19 +1,17 @@
 # !/bin/bash
 #
-port_number=50011
+port_number=50012
 category="medical"
-obj_name="chest"
-benchmark="Pneumothorax_Segmentation_Challenge"
-trigger_word='teeth'
+obj_name="brain"
+benchmark="NFBS"
+trigger_word='brain'
 layer_name='layer_3'
 sub_folder="up_16_32_64"
-file_name="1_pretrained_vae_pe_xray"
+file_name="4_unsupervised"
 
 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
  --main_process_port $port_number ../train.py --log_with wandb \
  --output_dir "../../result/${category}/${obj_name}/${layer_name}/${sub_folder}/${file_name}" \
- --network_weights "../../result/${category}/${obj_name}/${layer_name}/${sub_folder}/${file_name}/models/epoch-000001.safetensors" \
- --position_embedder_weights "../../result/${category}/${obj_name}/${layer_name}/${sub_folder}/${file_name}/position_embedder/position_embedder_1.safetensors" \
  --train_unet --train_text_encoder --start_epoch 1 --max_train_epochs 100 \
  --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
  --data_path "../../../MyData/anomaly_detection/${category}/${obj_name}/${benchmark}" \
@@ -27,5 +25,6 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
  --do_attn_loss --attn_loss_weight 1.0 --do_cls_train --normal_weight 1 \
  --min_perlin_scale 1 \
  --max_perlin_scale 4 \
- --max_beta_scale 0.8 \
- --min_beta_scale 0.4
+ --max_beta_scale 0.3 \
+ --min_beta_scale 0.1 \
+ --unsupervised
