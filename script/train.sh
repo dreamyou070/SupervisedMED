615f18a2b75a694194ed102d1688c7d9f9b7789b
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-03-11 17:08:21 +0900

    Update train.sh

--- script/train.sh
+++ script/train.sh
@@ -8,13 +8,16 @@ trigger_word='teeth'
 layer_name='layer_3'
 sub_folder="up_16_32_64"
 file_name="1_pretrained_vae_pe_xray"
+          "1_pretrained_vae_pe_xray_anomal"
 
 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
  --main_process_port $port_number ../train.py --log_with wandb \
- --output_dir "../../result/${bench_mark}/${layer_name}/${sub_folder}/${file_name}" \
- --train_unet --train_text_encoder --start_epoch 0 --max_train_epochs 100 \
+ --output_dir "../../result/${bench_mark}/${obj_name}/${layer_name}/${sub_folder}/${file_name}" \
+ --train_unet --train_text_encoder --start_epoch 1 --max_train_epochs 100 \
  --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
  --data_path "../../../MyData/anomaly_detection/${category}/${obj_name}/${benchmark}" \
+ --network_weights "../../result/${bench_mark}/${obj_name}/${layer_name}/${sub_folder}/${file_name}/models/epoch-000001.safetensors" \
+ --position_embedder_weights "../../result/${bench_mark}/${obj_name}/${layer_name}/${sub_folder}/${file_name}/position_embedder/position_embedder_1.safetensors" \
  --anomal_source_path "../../../MyData/anomal_source_l_mode" \
  --trigger_word "${trigger_word}" \
  --obj_name "${obj_name}" \
