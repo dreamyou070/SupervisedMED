0cc8febf0bc7c41de6c9572fc8fcbaa4a33642bf
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-03-11 20:44:47 +0900

    Update data_check.sh

--- script/data_check.sh
+++ script/data_check.sh
@@ -10,7 +10,7 @@ sub_folder="up_16_32_64"
 file_name="4_unsupervised"
 
 python ../data_check.py --log_with wandb \
- --output_dir "../../result/${category}/${obj_name}/${layer_name}/${sub_folder}/${file_name}/data_check_20240309_0.2_0.5" \
+ --output_dir "../../result/${category}/${obj_name}/${layer_name}/${sub_folder}/${file_name}/data_check_20240309_0.1_0.3" \
  --train_unet --train_text_encoder --start_epoch 0 --max_train_epochs 30 \
  --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
  --data_path "../../../MyData/anomaly_detection/${category}/${obj_name}/${benchmark}" \
@@ -19,5 +19,5 @@ python ../data_check.py --log_with wandb \
  --obj_name "${obj_name}" \
  --min_perlin_scale 1 \
  --max_perlin_scale 4 \
- --max_beta_scale 0.5 \
- --min_beta_scale 0.2 --unsupervised
+ --max_beta_scale 0.3 \
+ --min_beta_scale 0.1 --unsupervised
