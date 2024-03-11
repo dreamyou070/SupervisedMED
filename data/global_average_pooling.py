320c7f7d2accbd62d1c38f3635f64d7dbdf030db
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-03-11 16:42:02 +0900

    a

--- data/global_average_pooling.py
+++
@@ -1,9 +0,0 @@
-import torch
-from PIL import ImageEnhance, Image
-import numpy as np
-
-pil_path = '1.JPG'
-pil = Image.open(pil_path).convert('L')
-
-white_np = np.ones((512,512)) * 255
-Image.fromarray(white_np.astype(np.uint8)).show()
