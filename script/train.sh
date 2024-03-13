5d2532e2d415f378c70248a7ce3e537136afc673
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-03-13 11:05:07 +0900

    train with kaggle data

--- script/train.sh
+++ script/train.sh
@@ -2,25 +2,26 @@
 #
 port_number=50055
 category="medical"
-obj_name="chest"
-benchmark="Pneumothorax_Segmentation_Challenge_2"
+obj_name="dental"
+benchmark="kaggle"
 trigger_word='chest'
 layer_name='layer_3'
 sub_folder="up_16_32_64"
-file_name="2_supervised_normal_anormal_2_9"
-#--unsupervised
+file_name="22_supervised_kaggle"
+# --unsupervised
+# --anomal_source_path "../../../MyData/anomal_source_l_mode" \
+# --anomal_position_source_path "../../../MyData/random_shape/${obj_name}"
+
 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
  --main_process_port $port_number ../train.py --log_with wandb \
  --output_dir "../../result/${category}/${obj_name}/${layer_name}/${sub_folder}/${file_name}" \
  --train_unet --train_text_encoder --start_epoch 0 --max_train_epochs 100 \
  --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
  --data_path "../../../MyData/anomaly_detection/${category}/${obj_name}/${benchmark}" \
- --anomal_source_path "../../../MyData/anomal_source_l_mode" \
  --trigger_word "${trigger_word}" \
  --obj_name "${obj_name}" \
  --do_map_loss \
  --trg_layer_list "['up_blocks_1_attentions_2_transformer_blocks_0_attn2',
                     'up_blocks_2_attentions_2_transformer_blocks_0_attn2',
                     'up_blocks_3_attentions_2_transformer_blocks_0_attn2',]" \
- --do_attn_loss --attn_loss_weight 1.0 --do_cls_train --normal_weight 1 \
- --anomal_position_source_path "../../../MyData/random_shape/${obj_name}"
+ --do_attn_loss --attn_loss_weight 1.0 --do_cls_train --normal_weight 1
--- sy.py
+++
@@ -1,3 +0,0 @@
-image_paths = [3,4]
-anomaly_source_paths = [4,5,6]
-print(max(len(image_paths), len(anomaly_source_paths)))
