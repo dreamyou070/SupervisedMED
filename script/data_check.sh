df03c1925d04df74d963b63708f5a8e7704c3852
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-03-14 09:09:40 +0900

    Update data_check.sh

--- script/data_check.sh
+++ script/data_check.sh
@@ -8,11 +8,11 @@ benchmark="NFBS_preprocess"
 trigger_word='brain'
 layer_name='layer_3'
 sub_folder="up_16_32_64"
-file_name="6_unsupervised_skull_stripped_with_normal_sample_anomal_position"
+file_name="7_unsupervised_skull_stripped_with_normal_sample_anomal_position"
 #--do_self_aug
 
 python ../data_check.py --log_with wandb \
- --output_dir "../../result/${category}/${obj_name}/${layer_name}/${sub_folder}/${file_name}/data_check_anomal_simplex_noise_trg_beta_0.0_0.1_blur_2" \
+ --output_dir "../../result/${category}/${obj_name}/${layer_name}/${sub_folder}/${file_name}/data_check_anomal_simplex_noise_trg_beta_0.8_blur_2" \
  --train_unet --train_text_encoder --start_epoch 0 --max_train_epochs 30 \
  --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
  --data_path "../../../MyData/anomaly_detection/${category}/${obj_name}/${benchmark}" \
@@ -20,5 +20,5 @@ python ../data_check.py --log_with wandb \
  --anomal_source_path "../../../MyData/anomal_source_l_mode_neat" \
  --trigger_word "${trigger_word}" \
  --obj_name "${obj_name}" \
- --max_beta_scale 0.1 \
- --min_beta_scale 0.0 --unsupervised
+ --trg_beta 0.8 \
+ --unsupervised
