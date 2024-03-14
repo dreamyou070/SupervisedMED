4db119604daeda0b3bf03ede613a06e0d04eb953
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-03-15 07:30:58 +0900

    Update prepare_dataset.py

--- data/prepare_dataset.py
+++ data/prepare_dataset.py
@@ -17,10 +17,10 @@ def call_dataset(args) :
     dataset_class = TrainDataset
     if args.unsupervised :
         dataset_class = TrainDataset_Unsupervised
-        
     #if args.trigger_word == 'brain' :
     #    from data.dataset_brain import TrainDataset_Brain
     #    dataset_class = TrainDataset_Brain
+    
     dataset = dataset_class(root_dir=root_dir,
                             anomaly_source_path=args.anomal_source_path,
                             anomal_position_source_path=args.anomal_position_source_path,
