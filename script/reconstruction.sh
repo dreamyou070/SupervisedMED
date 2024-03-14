38e72123a332f61ae2f9a4fc674a5e4cfb7d3a58
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-03-15 08:04:27 +0900

    Update reconstruction.sh

--- script/reconstruction.sh
+++ script/reconstruction.sh
@@ -3,23 +3,19 @@
 port_number=50003
 category="medical"
 obj_name="brain"
-benchmark="BraTS2020_T1weighted"
-caption='brain'
+benchmark="NFBS_SkullStripped"
+trigger_word='brain'
 layer_name='layer_3'
 sub_folder="up_16_32_64"
-file_name="7_unsupervised_skull_stripped_with_normal_sample_anomal_position"
-
-# --use_position_embedder
-# --vae_pretrained_dir "/home/dreamyou070/SupervisedMED/result/Tuft/vae_train/train_vae_reconstruction_nomal_data/vae_models/vae_104.safetensors" \
-# --use_position_embedder
+file_name="8_unsupervised_skull_stripped_with_prepared_anomal"
 
 accelerate launch --config_file ../../../gpu_config/gpu_0_config \
  --main_process_port $port_number ../reconstruction.py \
  --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
  --network_dim 64 --network_alpha 4 \
- --data_path "../../../MyData/anomaly_detection/${category}/${obj_name}/${benchmark}/test" \
- --network_folder "../../result/${category}/${obj_name}/${layer_name}/${sub_folder}/${file_name}/models" \
-  --obj_name "${obj_name}" --prompt "${caption}" \
+ --data_path "/home/dreamyou070/MyData/anomaly_detection/${category}/${obj_name}/BraTS2020_T1weighted/test" \
+ --network_folder "/home/dreamyou070/SupervisedMED/result/${category}/${obj_name}/${layer_name}/${sub_folder}/${file_name}/models" \
+ --obj_name "${obj_name}" --prompt "${caption}" \
  --latent_res 64 \
  --trg_layer_list "['up_blocks_1_attentions_2_transformer_blocks_0_attn2',
                     'up_blocks_2_attentions_2_transformer_blocks_0_attn2',
